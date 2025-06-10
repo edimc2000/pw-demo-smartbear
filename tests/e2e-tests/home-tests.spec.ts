9e0a29c43738fcf4668448f99456ecab1f3a27fe
Eddie C. <edimc2000@gmail.com>
2025-06-09 21:41:05 -0500

    headless

--- tests/e2e-tests/home-tests.spec.ts
+++ tests/e2e-tests/home-tests.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Home Page verification', () => {
   test('Validate title and url', async({ page }) => {
-    console.log(await page.goto('').)
+    console.log(await page.goto(''))
     await page.goto('')
     expect(page.url().toLowerCase()).toContain('/weborders/');
     expect(await page.title()).toContain('Web Orders');
