2421f5e484a14245969f7c91167599e665e6ed3e
Eddie C. <edimc2000@gmail.com>
2025-06-09 22:46:29 -0500

    storage state logic to not login if file exists

--- tests/e2e-tests/home-tests.spec.ts
+++ tests/e2e-tests/home-tests.spec.ts
@@ -1,11 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Home Page verification', () => {
-  test('Validate title and url', async({ page }) => {
-   
+  
+  test('Validate title and url', async ({ page }) => {
+
     await page.goto('')
-    
     expect(page.url().toLowerCase()).toContain('/weborders/')
     expect(await page.title()).toContain('Web Orders')
+
+    // await page.waitForTimeout(5000)
   });
 });
--- tests/setup/login.spec.ts
+++ tests/setup/login.spec.ts
@@ -1,15 +1,42 @@
 import { test, expect } from '@playwright/test';
+import fs from 'fs'
 
-test('Login and save storage state', async ({ page }) => {
+test.describe('Sign In', () => {
 
-  await page.goto('');
+  const authFile: string = './auth/smartlogin.json'
+  if (checkIfFileExistsSync(authFile)) {
+    console.log('Storage State - Session - File exists!')
 
-  // await page.waitForTimeout(5000)
-  await page.getByRole('textbox', { name: 'Username:' }).fill('Tester');
-  await page.getByRole('textbox', { name: 'Password:' }).fill('test');
-  await page.getByRole('button', { name: 'Login' }).click();
+  } else {
+    console.log('Storage State - Session - does not exist. Creating a new one.');
+    signIn()
+  }
 
-  await expect(page.getByText('Welcome, Tester! | Logout')).toBeVisible();
 
-  await page.context().storageState({ path: './auth/smartlogin.json' });
-});
+  function checkIfFileExistsSync(authFile) {
+    try {
+      fs.accessSync(authFile, fs.constants.F_OK);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  function signIn() {
+    test('Login and save storage state', async ({ page }) => {
+
+      await page.goto('');
+      // await page.waitForTimeout(5000)
+      await page.getByRole('textbox', { name: 'Username:' }).fill('Tester');
+      await page.getByRole('textbox', { name: 'Password:' }).fill('test');
+      await page.getByRole('button', { name: 'Login' }).click();
+      await expect(page.getByText('Welcome, Tester! | Logout')).toBeVisible();
+
+      await page.context().storageState({ path: './auth/smartlogin.json' });
+
+      // await page.waitForTimeout(5000)
+    });
+  }
+
+})
+
