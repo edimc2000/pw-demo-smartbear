72d1bf65a32a5a0c9a5d5c2ad451e0363befe8a4
Eddie C. <edimc2000@gmail.com>
2025-06-09 22:04:46 -0500

    controlled config for headless option

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless:  process.env.CI ? true : false,
     /* Base URL to use in actions like `await page.goto('/')`. */
     //baseURL: process.env.baseURL,
 
