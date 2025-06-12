5fb2b8dbff0dabbbd34c18170359fa21a84fac11
Eddie C. <edimc2000@gmail.com>
2025-06-11 21:44:14 -0500

    updated golden sreen shot and  wait until on page load

--- tests/e2e-tests/home-tests.spec.ts
+++ tests/e2e-tests/home-tests.spec.ts
@@ -1,38 +1,36 @@
 // @ts-nocheck 
- 
- import { test, expect } from '@playwright/test';
+
+import { test, expect } from '@playwright/test';
 
 test.describe('Home Page verification', () => {
 
   test('Validate title and url', async ({ page }) => {
 
-    await page.goto('')
+    await page.goto('', { waitUntil: "domcontentloaded", })
     expect(page.url().toLowerCase()).toContain('/weborders/')
     expect(await page.title()).toContain('Web Orders')
 
-   
-    await expect(page).toHaveScreenshot({
+
+    await expect(page).toHaveScreenshot('HP1-Validation.png', {
       fullPage: true,
       clip: {
         x: 36,
         y: 6,
         width: 245,
         height: 290
-      }, 
-      timeout: 1000 
+      }
     })
 
 
 
-      await expect(page).toHaveScreenshot({
+    await expect(page).toHaveScreenshot('HP2-Validation.png', {
       fullPage: true,
       clip: {
         x: 302,
         y: 67,
         width: 925,
         height: 106
-      }, 
-      timeout: 1000 
+      }
     })
 
 
